df166c824cc4870c21062e736b4f48565904c609
Kamal Mohamed <117317040+kamal-sorour@users.noreply.github.com>
2026-01-29 01:38:19 +0200

    Fix import statement for FeaturedArticles component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from '../components/Home/Section1/Hero'
-import FeaturedArticles from '../components/Home/Section2/featuredArticles'
+import FeaturedArticles from '../components/Home/Section2/FeaturedArticles'
 import Categories from '../components/Home/Section3/Categories'
 import LatestArticles from '../components/Home/Section4/LatestArticles'
 import Subscribe from '../components/Home/Section5/Subscribe'
@@ -17,3 +17,4 @@ const Home = ({data}) => {
 }
 
 export default Home
+
